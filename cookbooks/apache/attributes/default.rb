ffa7aa0f3058da74c5b0e5e67ea5c62fc60689d4
root <root@workstation.(none)>
2016-11-02 20:59:56 +0000

    Commit chef-repo for the first time

---
+++ cookbooks/apache/attributes/default.rb
@@ -0,0 +1,10 @@
+default["apache"]["sites"]["sebrajesh2"] = { "site_title" => "sebrajesh2 website coming soon", "port" => 80, "domain" => "sebrajesh2.mylabserver.com" }
+default["apache"]["sites"]["sebrajesh2b"] = { "site_title" => "sebrajesh2b website coming soon!", "port" => 80, "domain" => "sebrajesh2b.mylabserver.com" }
+default["apache"]["sites"]["sebrajesh3"] = { "site_title" => "sebrajesh3 website", "port" => 80, "domain" => "sebrajesh3.mylabserver.com" }
+case node["platform"]
+when "centos"
+   default["apache"]["package"] = "httpd"
+when "ubuntu"
+   default["apache"]["package"] = "apache2"
+end
+    
---
+++ cookbooks/php/recipes/default.rb
@@ -0,0 +1,14 @@
+#
+# Cookbook Name:: php
+# Recipe:: default
+#
+# Copyright 2016, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+package "php" do
+  action :install
+end
+
+cookbook_file "/etc/php.ini" do
+    source "php.ini"
+end
